8ff468a44c4c40d1b25b061e5ade27a2fd999498
wendongfang <1781727127@qq.com>
2020-04-09 22:43:09 +0800

    添加MVP测试

--- app/src/main/java/com/example/hiot_cloud/MainActivity.java
+++ app/src/main/java/com/example/hiot_cloud/MainActivity.java
@@ -3,6 +3,13 @@ package com.example.hiot_cloud;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.hiot_cloud.test.mvptest.model.User;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("tag","test");
+
     }
+
+
+
+
 }
